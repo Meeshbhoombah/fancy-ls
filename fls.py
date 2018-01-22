954db7e649e2f24cf0b517634d549dc8242ddbe3
Rohan Mishra <rohanmishra2017@gmail.com>
2018-01-22 10:13:26 -0800

    Added baseline template for proj

---
+++ fls.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+"""
+fls.py
+
+Terminal command that expands on the functionality of the `ls` command by allowing tagged
+sorting of cateogries.
+"""
+
+import plistlib
+
